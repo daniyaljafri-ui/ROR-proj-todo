14d65d2df26ef0223c7e6ba8ad8cdde4437f09fe
daniyaljafri-ui <daniyal.jafri@camp1.tkxel.com>
2025-09-18 16:12:59 +0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Health check endpoint
+  get "up", to: ->(_env) { [200, { "Content-Type" => "text/plain" }, ["ok"]] }
   namespace :api do
     namespace :v1 do
       post "register", to: "auth#register"
